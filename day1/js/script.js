8225b28af1bee2e1c36eada60aa024bf0c2454c8
Sergey <seryngya@gmail.com>
2021-04-12 13:56:19 +0300

    first commit

---
+++ day1/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const answers = [];
+
+answers[0] = prompt("What is your name?");
+answers[1] = prompt("What is your second name?");
+answers[2] = prompt("What is your age?");
+
+console.log(answers);
+alert(answers[1]);
