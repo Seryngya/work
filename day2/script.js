b2ec981467933639b2616404e47074b63028b7ee
Sergey <seryngya@gmail.com>
2021-04-13 19:25:44 +0300

    day3

--- day2/script.js
+++ day2/script.js
@@ -1,5 +1,5 @@
+"use strict";
 let numberOfFilms = prompt("Count of films","number");
-console.log(numberOfFilms);
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -9,9 +9,54 @@ let personalMovieDB = {
     private: false
 };
 
-for (let index = 0; index < 2; index++) {
+for (let i = 0; i < 2; i++) {
     let film = prompt("Waht is last film you saw","name");
     let cost = prompt("Rate this film from 1 to 10","rate");
+    if( !(film.length > 0 && film.length < 50 && cost.length > 0 && cost.length < 50) ){
+        i--;
+        console.log("111");
+        continue;
+    }
     personalMovieDB.movies[film] = cost;
 }
+let q;
+if ( personalMovieDB.count < 10) {
+    q = 1;
+} else if ( personalMovieDB.count > 10 - 1 && personalMovieDB < 30){
+    q = 2;
+}else if ( personalMovieDB.count > 30 - 1){
+    q = 3;
+}
+
+switch (q) {
+    case 1: 
+            console.log('Просмотрено довольно мало фильмов');
+            break;
+    case 2:
+            console.log('Вы класический зритель');
+            break;
+    case 3:
+        console.log('Вы киноман');
+        break;
+    default: console.log('Error');
+
+}
+
+let w = counter();
+let e = counter();
+
+function counter() {
+    let count  = 0;
+    return function () {return ++count;};
+}
+ console.log(w());
+ console.log(w());
+ console.log(e());
+ console.log(e());
+ console.log(w());
+ console.log(w());
+  
+
+
 console.log(personalMovieDB);
+console.log(personalMovieDB.movies);
