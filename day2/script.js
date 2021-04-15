1d1b91f130c5b2344b21242e76e893495a591527
Sergey <seryngya@gmail.com>
2021-04-15 15:24:53 +0300

    day3

--- day2/script.js
+++ day2/script.js
@@ -1,5 +1,11 @@
 "use strict";
-let numberOfFilms = prompt("Count of films","number");
+let numberOfFilms;
+
+function start () {
+        while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы посмотрели?');
+        }
+}
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -8,7 +14,7 @@ let personalMovieDB = {
     genres: [],
     private: false
 };
-
+ function rememberMyFilm () {
 for (let i = 0; i < 2; i++) {
     let film = prompt("Waht is last film you saw","name");
     let cost = prompt("Rate this film from 1 to 10","rate");
@@ -19,6 +25,9 @@ for (let i = 0; i < 2; i++) {
     }
     personalMovieDB.movies[film] = cost;
 }
+}
+
+function detectPersonalLevel() {
 let q;
 if ( personalMovieDB.count < 10) {
     q = 1;
@@ -39,8 +48,37 @@ switch (q) {
         console.log('Вы киноман');
         break;
     default: console.log('Error');
-
+}
 }
 
+function showMyDB () {
+    if (!personalMovieDB.private){
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres () {
+    const arr = ['1 - govno', '2 - kal', '3 - cum'];
+    for (let i = 0; i < 3; i++) {
+        let answer = prompt('Ваш любимый жанр под номером', arr);
+        console.log(answer);
+        
+        switch (answer) {
+            case '1': 
+                personalMovieDB.genres.push(arr[0]);
+                break;
+            case '2':
+                personalMovieDB.genres.push(arr[1]);
+                break;
+            case '3':
+                personalMovieDB.genres.push(arr[2]);
+                break;
+            default:
+                 alert('Введите число от 1 до 3!!');
+                i--;
+        }
+    }
+}
+writeYourGenres();
 console.log(personalMovieDB);
 console.log(personalMovieDB.movies);
